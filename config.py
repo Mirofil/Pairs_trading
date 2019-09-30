28be9e8ad9138a4a2729b0c95ab27037dd15d56b
Miroslav-Fil <miroslav_fil@external.mckinsey.com>
2019-10-01 00:28:07 +0200

    and again..

--- config.py
+++ config.py
@@ -6,8 +6,8 @@ volume_cutoff = 0.7
 version='NEW'
 desc=['Mean', 'Total profit','Std', 'Sharpe','Sortino', 'VaR', 'Calmar',
      'Number of trades', 'Roundtrip trades', 'Avg length of position', 'Pct of winning trades', 'Max drawdown', 'Cumulative profit']
-data_path = version+r'concatenated_price_data\ '[:-1]
-save = version+r'results\ '[:-1]
-save_path_graphs = version+r'graphs\ '[:-1]
-save_path_tables = version+r'tables\ '[:-1]
+data_path = version+r'concatenated_price_data/ '[:-1]
+save = version+r'results/ '[:-1]
+save_path_graphs = version+r'graphs/ '[:-1]
+save_path_tables = version+r'tables/ '[:-1]
 NUMOFPROCESSES=35
